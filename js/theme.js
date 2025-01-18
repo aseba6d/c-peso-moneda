ed006a947e04ddebcc52d5e7315c70d38bf0e46f
Seba <142437362+aseba6d@users.noreply.github.com>
2025-01-17 23:10:35 -0300

    Update

--- js/theme.js
+++ js/theme.js
@@ -1,33 +1,45 @@
 const fecha = new Date();
 const urlTheme = document.getElementById("csstheme");
+const urlParams = new URLSearchParams(window.location.search);
+let urlVar = urlParams.get("t");
+if(urlVar === null) { urlVar = 0};
 let datostheme = [
     {
         name: "asebabd",
         file: "css/asebabd.css",
-        incio: { dia: 1, mes: 1, year: 2025, hora: '00:00:00' },
-        fin: { dia: 31, mes: 12, year: 2025, hora: '23:59:59' }
+        type: 'Url',
+        urlCodigo: 0
     },
     {
         name: "Fiesta Patrias",
         file: "css/18Sept.css",
-        incio: { dia: 1, mes: 9, year: 2025, hora: '00:00:00' },
-        fin: { dia: 30, mes: 9, year: 2025, hora: '23:59:59' }
+        type: 'fecha',
+        incio: { dia: 1, mes: 9, hora: '00:00:00' },
+        fin: { dia: 30, mes: 9, hora: '23:59:59' },
+        urlCodigo: 1
     },
     {
         name: "Navidad",
         file: "css/navidad.css",
-        incio: { dia: 1, mes: 12, year: 2025, hora: '00:00:00' },
-        fin: { dia: 31, mes: 12, year: 2025, hora: '23:59:59' }
+        type: 'fecha',
+        incio: { dia: 1, mes: 12, hora: '00:00:00' },
+        fin: { dia: 31, mes: 12, hora: '23:59:59' },
+        urlCodigo: 2
     }
 ];
 
 function changetheme() {
-    let xy, yz;
+    urlTheme.href = datostheme[0].file;
     datostheme.forEach(temas => {
-        xy = new Date(`${fecha.getFullYear()}/${temas.incio.mes}/${temas.incio.dia} ${temas.incio.hora}`);
-        yz = new Date(`${fecha.getFullYear()}/${temas.fin.mes}/${temas.fin.dia} ${temas.fin.hora}`);
-        if ((xy <= fecha) && (yz >= fecha)) {
-            urlTheme.href = temas.file;
+        if ((parseInt(urlVar) === temas.urlCodigo) && (parseInt(urlVar) < datostheme.length)) {
+                urlTheme.href = temas.file;
+        } 
+        if (temas.type === 'fecha') {
+            let xy = new Date(`${fecha.getFullYear()}/${temas.incio.mes}/${temas.incio.dia} ${temas.incio.hora}`);
+            let yz = new Date(`${fecha.getFullYear()}/${temas.fin.mes}/${temas.fin.dia} ${temas.fin.hora}`);
+            if ((xy <= fecha) && (yz >= fecha)) {
+                urlTheme.href = temas.file;
+            }
         }
     });
 }
