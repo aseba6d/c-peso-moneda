ef572584b2e0c4002af48e2ff21102faaf384044
Seba <142437362+aseba6d@users.noreply.github.com>
2025-01-31 19:48:58 -0300

    correccion de errores

--- js/theme.js
+++ js/theme.js
@@ -4,105 +4,103 @@ const urlParams = new URLSearchParams(window.location.search);
 let urlVar1 = urlParams.get("tema");
 let urlVar2 = urlParams.get("on");
 let urlVar3 = urlParams.get("off");
-let FechaCdg, DefCdg, UrlCdg;
+let FechaCdg;
+const DefCdg = [];
+const UrlCdg = [], UrlCdg2  = [];
 let datostheme = [
     {
         name: "asebabd",
         file: "css/asebabd.css",
-        type: 'Url',
-        urlCodigo: 0,
-        default: true
+        type: ['default', 'Url'],
+        urlCodigo: 0
     },
     {
         name: "Fiesta Patrias",
         file: "css/18Sept.css",
-        type: 'fecha',
+        type: ['fecha'],
         incio: { dia: 1, mes: 9 },
-        fin: { dia: 30, mes: 9 },
-        urlCodigo: 1,
-        default: false
+        fin: { dia: 30, mes: 9 }
     },
     {
         name: "Navidad",
         file: "css/navidad.css",
-        type: 'fecha',
+        type: ['fecha'],
         incio: { dia: 1, mes: 12 },
-        fin: { dia: 31, mes: 12 },
-        urlCodigo: 2,
-        default: false
-    },
-    {
-        name: "Hello Kitty",
-        file: "css/theme-3.css",
-        type: 'Url',
-        urlCodigo: 3,
-        default: false
+        fin: { dia: 31, mes: 12 }
     },
     {
         name: "Pokémon",
         file: "css/theme-4.css",
-        type: 'Url',
-        urlCodigo: 4,
-        default: false
+        type: ['Url'],
+        urlCodigo: 1
     },
     {
         name: "SlamDunk",
         file: "css/theme-5.css",
-        type: 'Url',
-        urlCodigo: 5,
-        default: false
+        type: ['Url'],
+        urlCodigo: 2
+    },
+    {
+        name: "Hello Kitty",
+        file: "css/theme-3.css",
+        type: ['Url'],
+        urlCodigo: 3
     },
     {
         name: "DragonBall Z",
         file: "css/theme-6.css",
-        type: 'Url',
-        urlCodigo: 6,
-        default: false
-    },
-    {
-        name: "Inuyasha",
-        file: "css/theme-7.css",
-        type: 'Url',
-        urlCodigo: 7,
-        default: false
+        type: ['Url'],
+        urlCodigo: 4
     },
     {
         name: "Liliana",
         file: "css/liliana.css",
-        type: 'fecha',
+        type: ['default', 'fecha', 'Url'],
         incio: { dia: 29, mes: 1 },
         fin: { dia: 9, mes: 2 },
-        urlCodigo: 8,
-        default: false
+        urlCodigo: 5
     },
     {
         name: "Sebastian",
         file: "css/sebastian.css",
-        type: 'fecha',
+        type: ['default', 'fecha', 'Url'],
         incio: { dia: 31, mes: 1 },
         fin: { dia: 31, mes: 1 },
-        urlCodigo: 9,
-        default: false
+        urlCodigo: 6
+    },
+    {
+        name: "Inuyasha",
+        file: "css/theme-7.css",
+        type: ['Url'],
+        urlCodigo: 7
     }
 ];
-datostheme.forEach(temas => {
-    if (temas.type === 'Url' && temas.default === true) {
-        DefCdg = temas.urlCodigo;
+datostheme.forEach((temas, idx) => {
+    if (temas.type.indexOf('default') !== -1) {
+        DefCdg.push(temas.file);
     }
-    if (temas.type === 'fecha') {
+    if (temas.type.indexOf('fecha') !== -1) {
         let xy = new Date(`${fecha.getFullYear()}/${temas.incio.mes}/${temas.incio.dia} 00:00:00`);
         let yz = new Date(`${fecha.getFullYear()}/${temas.fin.mes}/${temas.fin.dia} 23:59:59`);
         if (xy <= fecha && yz >= fecha) {
             FechaCdg = temas.urlCodigo;
         }
     }
+    if (temas.type.indexOf('Url') !== -1) {
+        UrlCdg.push(temas.urlCodigo);
+        UrlCdg2.push(idx);
+    }
 });
 if ((urlVar2 === '' || urlVar2 === null) && urlVar3 !== '' && FechaCdg !== undefined) {
     urlTheme.href = datostheme[FechaCdg].file;
-} else if (parseInt(urlVar1) < datostheme.length && (urlVar1 !== null || urlVar1 !== '')) {
-    urlTheme.href = datostheme[parseInt(urlVar1)].file;
+} else if (parseInt(urlVar1) < UrlCdg.length && (urlVar1 !== null || urlVar1 !== '') && (UrlCdg.indexOf(parseInt(urlVar1)) !== -1)) {
+    urlTheme.href = datostheme[UrlCdg2[UrlCdg.indexOf(parseInt(urlVar1))]].file;
+
 } else {
-    urlTheme.href = datostheme[DefCdg].file;
+    urlTheme.href = DefCdg[getRandomInt(DefCdg.length)];
 }
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
 /* function changetheme() {};
 window.addEventListener('load', changetheme); */
